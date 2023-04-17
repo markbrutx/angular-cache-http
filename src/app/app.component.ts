a7746db2c37e3887321fab50504911f3babf9b2d
markbrutx <themarkbrut@gmail.com>
2023-04-17 23:37:17 +0600

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-cache-http';
+
+
+  constructor(private http: HttpClient) {}
+
+  public getUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
